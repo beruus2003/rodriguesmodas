1edc560cac5365be890309bbc0d74d9a60c7dd82
beruus2003 <212657786+beruus2003@users.noreply.github.com>
2025-09-02 00:22:46 -0300

    Update use-auth.ts

--- rodrigues-modas-github-render-clean/client/src/hooks/use-auth.ts
+++ rodrigues-modas-github-render-clean/client/src/hooks/use-auth.ts
@@ -1,3 +1,1 @@
-// client/src/hooks/use-auth.ts
-// Este arquivo agora apenas exporta o hook do nosso novo contexto.
-export { useAuthFromContext as useAuth } from '../contexts/AuthContext';
+export { useAuth } from '../contexts/AuthContext';
