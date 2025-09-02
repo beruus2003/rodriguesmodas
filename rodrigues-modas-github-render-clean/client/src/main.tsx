c5b611691574711cfee8527eefc5575d9f8f3ede
beruus2003 <212657786+beruus2003@users.noreply.github.com>
2025-09-02 12:20:13 -0300

    Update main.tsx

--- rodrigues-modas-github-render-clean/client/src/main.tsx
+++ rodrigues-modas-github-render-clean/client/src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App"
+import "./index.css"
+import { ToastProvider } from "@/components/ui/use-toast"
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ToastProvider>
+      <App />
+    </ToastProvider>
+  </React.StrictMode>
+)
